8e4c43f35795f5b078543847886fb2aed71dc021
Adin Ackerman <adinackerman@gmail.com>
2024-10-27 10:29:16 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CodeX",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CodeX",
+            targets: ["CodeX"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CodeX"),
+        .testTarget(
+            name: "CodeXTests",
+            dependencies: ["CodeX"]
+        ),
+    ]
+)
